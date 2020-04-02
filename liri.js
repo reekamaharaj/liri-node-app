93e1af7d92d08d055e3c75f9de95da3f1f8ff98a
Reeka <reekamaharaj@gmail.com>
2020-04-01 21:54:36 -0500

    concert, song and movie functions working for everything except the case where nothing is entered

--- liri.js
+++ liri.js
@@ -19,6 +19,7 @@ let song = undefined;
 let artists = undefined;
 let output;
 
+
 switch(command){
     case "concert-this":
             if (param === undefined){
@@ -33,7 +34,7 @@ switch(command){
     
     case "spotify-this-song":
             if (param === undefined){
-                song = "the sign";
+                song = "thesign";
             }
             else {
                 song = param.join("");
@@ -43,7 +44,7 @@ switch(command){
 
     case "movie-this":
             if (param === undefined){
-                movie = "Mr.+Nobody";
+                movie = "Mr.Nobody";
             }
             else {
                 movie = param.join("+");
@@ -57,6 +58,7 @@ switch(command){
             //takes the text from inside the random.txt file and uses it to call a LIRI command
 }
 
+//Again works for anything entered but not for the case where no input is given
 function concertThis(){
     let queryUrlBands = "https://rest.bandsintown.com/artists/" + artists + "/events?app_id=codingbootcamp";
 
@@ -66,13 +68,12 @@ function concertThis(){
                 output = "Venue: " + response.data[i].venue.name + ", Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY");
                 console.log(output);
 
-                fs.appendFile("log.txt", ", " + output, function(err) {
-                    if (err) {
-                        return console.log(err);
+                fs.appendFile("log.txt", ", " + output, function(error) {
+                    if (error) {
+                        return console.log(error);
                     }
                 });
             }
-
             }).catch(function(error) {
                 if (error) {
                     console.log(error.config);
@@ -81,9 +82,10 @@ function concertThis(){
             artists = undefined;
 }
 
+//works for songs entered but not for the default case. Probably the same problem that is happening for the movie function
 function songThis(){
     spotify.search({ type: 'track', query: song}).then(function(response) {
-        for(var i=0; i < 10; i++){
+        for(var i=0; i < 4; i++){
             output = "Artist: " + response.tracks.items[i].artists[0].name + " Song name: " + response.tracks.items[i].name + " Preview URL:  " + response.tracks.items[i].preview_url + " Album the song is on: " + response.tracks.items[i].album.name;
             console.log(output);
 
@@ -99,12 +101,16 @@ function songThis(){
     song = undefined;
 }
 
+//works but not for mr. nobody unless its manually entered without a space. but will accept other movie titles with multiple words. need to figure out what to do with the rotten tomatoes rating. Sometimes works but isn't on every movie, and it stops working. Might need an if loop to handle that. 
+
 function movieThis(){
     let queryUrlOmdb = "http://www.omdbapi.com/?t="+ movie +"&y=&plot=short&apikey=trilogy";
         axios.get(queryUrlOmdb).then(
             function(response) {
-                console.log(response.data);
-                output = "Movie Title: " + response.data.Title + ", Movie Release Year: " + response.data.Year + ", IMDB Rating: " + response.data.imdbRating + ", Rotten Tomatoes rating: " + response.data.Ratings[1].Value + ", Movie produced in: " + response.data.Country + "Language(s): " + response.data.Language + ", Movie plot: " + response.data.Plot + ", Actors: " + response.data.Actors;
+                output = "Movie Title: " + response.data.Title + ", Movie Release Year: " + response.data.Year + ", IMDB Rating: " + response.data.imdbRating + ", Movie produced in: " + response.data.Country + "Language(s): " + response.data.Language + ", Movie plot: " + response.data.Plot + ", Actors: " + response.data.Actors;
+
+                // rating = ", Rotten Tomatoes rating: " + response.data.Ratings[1].Value;
+
                 console.log(output);
 
                 fs.appendFile("log.txt", ", " + output, function(err) {
