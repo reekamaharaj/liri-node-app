94bd904b1c1c5f59a96a895065a399e9ba9c612c
Reeka <reekamaharaj@gmail.com>
2020-04-01 21:20:10 -0500

    movie search will take multiple words for a title now

--- liri.js
+++ liri.js
@@ -1,20 +1,19 @@
-require("dotenv").config();
 
-//TODO: Need to set the default song and movie if nothing is given
 //TODO: Need to format log.txt
 //TODO: Write code to make this do something with the data it reads
 
+require("dotenv").config();
 const fs = require("fs");
 const keys = require("./keys.js");
 const moment = require("moment");
 const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
-
 const axios = require("axios");
 
-let input = process.argv;
-let command = input[2];
-let param = input[3];
+let input = process.argv.slice(2);
+let command = input[0];
+let param = input.slice(1);
+
 let movie = undefined;
 let song = undefined;
 let artists = undefined;
@@ -22,35 +21,34 @@ let output;
 
 switch(command){
     case "concert-this":
-        if (artists === undefined){
-            artists = "tool";
-        }
-        else {
-            artists = param;
-        }
-        concert();
+            if (param === undefined){
+                artists = "tool";
+            }
+            else {
+                
+                artists = param.join("");
+            }
+            concertThis();
         break;
     
     case "spotify-this-song":
-        if (song === undefined){
-            song = "the sign";
-        }
-        else {
-            artists = param;
-        }
-        song();
-
+            if (param === undefined){
+                song = "the sign";
+            }
+            else {
+                song = param.join("");
+            }
+            songThis();
         break;
 
     case "movie-this":
-        if (movie === undefined){
-            movie = "mr. nobody";
-        }
-        else {
-            movie = param;
-        }
-        movie();
-
+            if (param === undefined){
+                movie = "Mr.+Nobody";
+            }
+            else {
+                movie = param.join("+");
+            }
+            movieThis();
         break;
 
         case "do-what-it-says":
@@ -59,12 +57,12 @@ switch(command){
             //takes the text from inside the random.txt file and uses it to call a LIRI command
 }
 
-function concert(){
+function concertThis(){
     let queryUrlBands = "https://rest.bandsintown.com/artists/" + artists + "/events?app_id=codingbootcamp";
 
         axios.get(queryUrlBands).then(
             function(response) {
-                for(var i=0; i < 5; i++){
+                for(var i=0; i < 4; i++){
                 output = "Venue: " + response.data[i].venue.name + ", Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY");
                 console.log(output);
 
@@ -76,56 +74,36 @@ function concert(){
             }
 
             }).catch(function(error) {
-                if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
-                    console.log("---------------Data---------------");
-                    console.log(error.response.data);
-                    console.log("---------------Status---------------");
-                    console.log(error.response.status);
-                    console.log("---------------Status---------------");
-                    console.log(error.response.headers);
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                    console.log(error.request);
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log("Error", error.message);
+                if (error) {
+                    console.log(error.config);
                 }
-                console.log(error.config);
             });
             artists = undefined;
 }
 
-function song(){
+function songThis(){
     spotify.search({ type: 'track', query: song}).then(function(response) {
-
-        for(var i=0; i < 5; i++){
-
+        for(var i=0; i < 10; i++){
             output = "Artist: " + response.tracks.items[i].artists[0].name + " Song name: " + response.tracks.items[i].name + " Preview URL:  " + response.tracks.items[i].preview_url + " Album the song is on: " + response.tracks.items[i].album.name;
             console.log(output);
 
-            fs.appendFile("log.txt", ", " + output, function(err) {
-                if (err) {
-                    return console.log(err);
+            fs.appendFile("log.txt", ", " + output, function(error) {
+                if (error) {
+                    return console.log(error);
                 }
             });
-
         }
-        
-
-    }).catch(function(err) {
-        console.log(err);
+    }).catch(function(error) {
+        console.log(error);
     });
     song = undefined;
 }
 
-function movie(){
+function movieThis(){
     let queryUrlOmdb = "http://www.omdbapi.com/?t="+ movie +"&y=&plot=short&apikey=trilogy";
-        
         axios.get(queryUrlOmdb).then(
             function(response) {
+                console.log(response.data);
                 output = "Movie Title: " + response.data.Title + ", Movie Release Year: " + response.data.Year + ", IMDB Rating: " + response.data.imdbRating + ", Rotten Tomatoes rating: " + response.data.Ratings[1].Value + ", Movie produced in: " + response.data.Country + "Language(s): " + response.data.Language + ", Movie plot: " + response.data.Plot + ", Actors: " + response.data.Actors;
                 console.log(output);
 
@@ -135,55 +113,22 @@ function movie(){
                     }
                 });
             }).catch(function(error) {
-                if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
-                    console.log("---------------Data---------------");
-                    console.log(error.response.data);
-                    console.log("---------------Status---------------");
-                    console.log(error.response.status);
-                    console.log("---------------Status---------------");
-                    console.log(error.response.headers);
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                    console.log(error.request);
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log("Error", error.message);
+                if (error) {
+                    console.log(error);
                 }
-                console.log(error.config);
             });
         movie = undefined;
 }
 
 function random(){
-    fs.readFile("random.txt", "utf8", function(err, data){
-        let random = data.split(",");
-        // command = data[0];
-        song = data[1]
-        //run the spotify search
-        spotify.search({ type: 'track', query: song}).then(function(response) {
-
-            for(var i=0; i < 5; i++){
-
-                output = "Artist: " + response.tracks.items[i].artists[0].name + " Song name: " + response.tracks.items[i].name + " Preview URL:  " + response.tracks.items[i].preview_url + " Album the song is on: " + response.tracks.items[i].album.name;
-                console.log(output);
-
-                fs.appendFile("log.txt", ", " + output, function(err) {
-                    if (err) {
-                        return console.log(err);
-                    }
-                });
-
-            }
-            
-
-        }).catch(function(err) {
-            console.log(err);
-        });
-        if (err) {
-            return console.log(err);
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if (error) {
+            console.error('There was an error reading the file!', err);
+            return;
         }
+        let randomTxt = data.split(",");
+        command = randomTxt[0];
+        param = randomTxt[1];
+        console.log("Command was " + command + " Input was " + param);
     })
 }
